000b7a9d76b545e78c8f6b0c60bf3fc98b401d43
cl1987 <2630636922@qq.com>
2019-07-25 07:51:24 +0800

    eyes

--- Nodejs/01-module/core/http.js
+++ Nodejs/01-module/core/http.js
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
+
 // const http=require('http');
 // console.log(http)
 // console.log('http.........')
 
 const http=require('http');
-=======
-const http = require('http');
 
->>>>>>> 936c8c74a1e0004b672ee8054db21e18699d1d26
+const http = require('http');
 console.log(http)
